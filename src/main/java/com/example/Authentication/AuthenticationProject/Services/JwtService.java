f0634a14ddb2a6499328128069f2a8ab083758a2
Samaan Kamal <samaankamal72@gmail.com>
2024-03-08 17:24:50 +0200

    added service to handle all jwt services functions

---
+++ src/main/java/com/example/Authentication/AuthenticationProject/Services/JwtService.java
@@ -0,0 +1,36 @@
+package com.example.Authentication.AuthenticationProject.Services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secretKey}")
+    private String secretKey;
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims=extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
