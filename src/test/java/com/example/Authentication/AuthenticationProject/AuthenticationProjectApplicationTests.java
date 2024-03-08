883454c61512a4b5ffabd572043788cb02d63f38
Samaan Kamal <samaankamal72@gmail.com>
2024-03-08 17:23:22 +0200

    intialized dependacies

---
+++ src/test/java/com/example/Authentication/AuthenticationProject/AuthenticationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Authentication.AuthenticationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
