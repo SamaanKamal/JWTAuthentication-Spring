386242511530e8172f8df5385d4a179cabb5a916
Samaan Kamal <samaankamal72@gmail.com>
2024-03-10 01:18:22 +0200

    added find by email method in repository

--- src/main/java/com/example/Authentication/AuthenticationProject/Repository/UserRepository.java
+++ src/main/java/com/example/Authentication/AuthenticationProject/Repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.Authentication.AuthenticationProject.Repository;
 import com.example.Authentication.AuthenticationProject.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Users,Integer> {
+    Optional<Users> findByEmail(String email);
 }
