266a55354e74ef4cad93405d71ed3080b5a310bc
Samaan Kamal <samaankamal72@gmail.com>
2024-03-10 01:19:35 +0200

    added security configuration class to premit certain requests and authenticate other requests

---
+++ src/main/java/com/example/Authentication/AuthenticationProject/Config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.Authentication.AuthenticationProject.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+                .sessionManagement((session) -> session
+                .sessionCreationPolicy(STATELESS)).authenticationProvider(authenticationProvider).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                ;
+        return http.build();
+    }
+}
