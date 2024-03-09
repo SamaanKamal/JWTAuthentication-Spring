d8522f3bcfe235b871972985d74a7843688301ce
Samaan Kamal <samaankamal72@gmail.com>
2024-03-10 01:24:26 +0200

    solved compitable dependacies issue

--- src/main/java/com/example/Authentication/AuthenticationProject/Entity/Users.java
+++ src/main/java/com/example/Authentication/AuthenticationProject/Entity/Users.java
@@ -1,10 +1,7 @@
 package com.example.Authentication.AuthenticationProject.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +14,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "user")
 public class Users implements UserDetails {
     @Id
